dad8b53b027a055bca634c1412274ea04857de2e
Edu Bezold <ebezold@misstipsi.com>
2022-09-06 11:14:41 -0500

    Initial data model

---
+++ Quipu.ShoppingPlan/Quipu.ShoppingPlan.Repository/Model/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace Quipu.ShoppingPlan.Repository.Model
+{
+    public class Article
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual IEnumerable<Tag> Tags { get; set; }
+    }
+}
---
+++ Quipu.ShoppingPlan/Quipu.ShoppingPlan.Repository/Model/ShoppingList.cs
@@ -0,0 +1,10 @@
+﻿namespace Quipu.ShoppingPlan.Repository.Model
+{
+    public class ShoppingList
+    {
+        public Guid Id { get; set; }
+        public bool IsPurchased { get; set; }
+
+        public virtual IEnumerable<Article> Articles { get; set; }
+    }
+}
